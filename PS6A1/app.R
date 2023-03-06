434f11f82d687f69109581756f69ccbe4b0fc55b
Thalia Felice <tfelice@uw.edu>
2023-03-05 22:19:41 -0800

    working files to gitHub

---
+++ PS6A1/app.R
@@ -0,0 +1,52 @@
+
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+setwd("/Users/thaliafelice/Desktop/info201/ps/ps6/data/PS6A1")
+bikes <- read_csv("bikes.csv")
+
+ui <- fluidPage(
+    titlePanel("PS6 Assignment"),
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("month",
+                    "month of the year", 
+                    value = 6,
+                    min = 1,
+                    max = 12),
+        fluidRow(
+          column(6,
+                 radioButtons("color", "Choose Graphing Color", 
+                              choices = c("thistle", "aquamarine","brown",
+                                          "maroon", "blue", "orange"))),
+        )),
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Summary",mainPanel(
+                    p("This is a summary paragraph")
+                    )),
+                    tabPanel("Plot", plotOutput("plot")),
+                    tabPanel("Table", mainPanel("table"))
+              )        
+            )      
+        )
+      )
+ 
+server <- function(input, output) {
+  bikemonth <- reactive({
+    bikes %>% 
+      filter(month %in% input$month)
+  })
+  
+  output$plot <- renderPlot({ # plot code goes here
+    bikemonth() %>% 
+      ggplot()+
+      geom_line(aes(num_bikes_rented, solar_radiation,col=input$color))
+  })
+}  
+
+shinyApp(ui = ui, server = server)
+
+
+
