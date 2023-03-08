33963ea50c695292f49f6f829bb57c422d9d091f
Thalia Felice <tfelice@uw.edu>
2023-03-07 19:13:13 -0800

    final touches and user doc

--- PS6A1/app.R
+++ PS6A1/app.R
@@ -3,7 +3,6 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
-setwd("/Users/thaliafelice/Desktop/ps6/PS6A1")
 bikes <- read_csv("bikes2.csv")
 
 ui <- fluidPage(
@@ -12,20 +11,27 @@ ui <- fluidPage(
       sidebarPanel(
        
         sliderInput("month",
-                    "month of the year", 
+                    "Select Month of the Year", 
                     value = 6,
                     min = 1,
                     max = 12),
+        
+        sliderInput("column", "Select Column Number in the Table",
+                    value = 3,
+                    min = 2,
+                    max = 7),
+                    
+        
         fluidRow(
           column(4,
-                 radioButtons("color", "Choose Graphing Color", 
+                 radioButtons("color", "Choose one to Select Graphing Color", 
                               choices = c("maroon", "brown",
                                           "blue", "orange"))),
         
         ),
                                           
           
-        h3("Key"),
+        h3("Month Number Key"),
         p(strong(1), "- January"),
         p(strong(2), "- February"),
         p(strong(3), "- March"),
@@ -52,31 +58,41 @@ ui <- fluidPage(
           and was collected from January 2017 to November 2018. Specifically, 
           I'm interested in what affect solar radiation (or in other words the presenceo of the sun or a 'nice day') may increase or decrease
           bike rentals. Before I started analyzing the data, my hypothesis was that higher amounts of solar radiation would lead to higher numbers of bike rentals, 
-          becuase people would want to bike to enjoy a sunny day. However, I began to see something different."),
+          because people would want to bike to enjoy a sunny day. However, I began to see something different."),
           h2("Findings"),
           p("In comparing the amount of solar radiation to bike rentals I found very little to no correlation. No graph for any month showed enough clustering or grouping 
             of the data points to suggest that there was a strong relationship between the two. One interesting characteristic I did see in the graphs was that the range of bike rentals between months
             increased from month 1 until month 7. After month 7, the range began to decrease each month."),
           h2("Conclusion"),
-          p("Though I hypothesized a positive relationship between solar radiation and bike rentals, the data disproves this hypothesis. There is no correlation between the variables.")
+          p("Though I hypothesized a positive relationship between solar radiation and bike rentals, the data disproves this hypothesis. There is no correlation between the variables."),
+          tableOutput("full")
           )),
           
         tabPanel("Plot", mainPanel(
+          h1("Scatterplot of the Data"),
           plotOutput("plot"),
           textOutput("plotObservations")
          )),
         
           tabPanel("Table", mainPanel(
+            h1("Summary Value Table of the Data"),
             tableOutput("table"),
             textOutput("median_bikes"),
-            textOutput("median_solar")
+            textOutput("median_solar"),
+            h2("Column Number Key"), 
+            p(strong(2), "- Median Number of Bikes Rented in the Month on a Given Day"),
+            p(strong(3), "- Median Amount of Solar Radiation in the Month on a Given Day"),
+            p(strong(4), "- Minimum Number of Bikes Rented in the Month on a Given Day"),
+            p(strong(5), "- Maximum Number of Bikes Rented in the Month on a Given Day"),
+            p(strong(6), "- Minimum Amount of Solar Radiation in the Month on a Given Day"),
+            p(strong(5), "- Maximum Amount of Solar Radiation in the Month on a Given Day")
+            
                       ))
                              
               )        
             )      
-        )
-)  
- 
+    )  
+)
 server <- function(input, output) {
   
   bikemonth <- reactive({
@@ -95,8 +111,17 @@ server <- function(input, output) {
     table <- bikes %>% 
       filter(!is.na(solar_radiation), !is.na(num_bikes_rented), month %in% input$month) %>% 
       group_by(month) %>% 
-      summarize(median_bikes = median(num_bikes_rented), median_solar = median(solar_radiation), min_bikes = min(num_bikes_rented), max_bikes = max(num_bikes_rented), min_solar = min(solar_radiation), max_solar = max(solar_radiation)) 
+      summarize(median_bikes = median(num_bikes_rented), median_solar = median(solar_radiation), min_bikes = min(num_bikes_rented), max_bikes = max(num_bikes_rented), min_solar = min(solar_radiation), max_solar = max(solar_radiation)) %>% 
+      select(input$column)
+      
     })
+  
+  output$full <- renderTable({
+    bikes %>% 
+      filter(!is.na(solar_radiation), !is.na(num_bikes_rented)) %>% 
+      group_by(month) %>% 
+      summarize(median_bikes = median(num_bikes_rented), median_solar = median(solar_radiation), min_bikes = min(num_bikes_rented), max_bikes = max(num_bikes_rented), min_solar = min(solar_radiation), max_solar = max(solar_radiation))
+  })
   
   output$plotObservations <- renderText({
     x <- bikes %>% 
@@ -125,7 +150,7 @@ server <- function(input, output) {
     
   })
   
-  output$value <- renderPrint({ input$checkbox })
+
 
 }  
 
--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# ps6
+# User Documentation 
 
-# User documentation 
+## Description of the Data
+My dataset comes from UCI Machine Learning Repository and it is what I will be using for the final group project. The data is from Seoul, south Korea and was collected from January 2017 to November 2018. Specifically, I'm interested in what affect solar radiation (or in other words the presenceo of the sun or a 'nice day') may increase or decrease bike rentals. Before I started analyzing the data, my hypothesis was that higher amounts of solar radiation would lead to higher numbers of bike rentals, because people would want to bike to enjoy a sunny day. However, I began to see something different. In comparing the amount of solar radiation to bike rentals I found very little to no correlation. No graph for any month showed enough clustering or grouping of the data points to suggest that there was a strong relationship between the two. 
 
-The data comes from 
+## Explanation of Widgets and Panels 
+
+There are three interactive widgets on my shiny app: 
+
+### Slider: Select Month of the Year
+
+This slider is the most important widget for my shiny app as it changes the output on both the plot and table pages. Each number, 1 - 12, corresponds to a month of the year. In the data set, I had to separate the month, day and year into three separate columns and delete the zero which proceeded all months which corresponded to a single digit number. On both the plot and tbale pages, this slider is used to filter between data sets. 
+
+### Slider: Select Column Number in the Table
+
+This slider is used on the table page and was included to fulfill the requirement of having a second widget impacting the table. While the first slider filters for a specific row of values about a month, the second slider filters by column, thus only displaying one value at a time rather than a row. I included a key on the table page so users can better understand how each number, 2-7, corresponds with the table. The full table is also printed on the overview page for clarity. 
+
+### Radio Buttons: Choose one to Select Graphing Color
+
+These buttons change the graphing color of the plot without changing how the data is structured or rerunning the plot. 
+
+### Side Panel Key 
+
+Because I am referencing months by numeric values, and while I hoped this would be intuitive, I thought it would be safest to include a key on all pages so users understood what exactly I was referencing. 
+
+# Link to Shiny App 
+
+[Here is the link to my working shiny app](https://tfelice25.shinyapps.io/PS6A1/)
